bb3901713a222dc668215bce26b7930c0413fe99
Brian Nguyen <vbrian@gmaial.com>
2023-06-09 09:20:53 -0700

    Update text color

--- app/components/Product/ProductForm.js
+++ app/components/Product/ProductForm.js
@@ -126,7 +126,7 @@ const ProductForm = ({ content, setImagesArray }) => {
                   pl-3
                   pr-10
                   py-2
-                  text-base
+                  text-black
                   border-gray-300
                   focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
                   sm:text-sm
