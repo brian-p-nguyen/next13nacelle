f6848ee0239372f5ed41e5fa9a3d42f9f2e69450
Brian Nguyen <vbrian@gmaial.com>
2023-06-20 10:03:21 -0700

    Add new route to test axios

---
+++ app/products/(nonacelle)/nonacelleaxios/[handle]/getShopifyData.js
@@ -0,0 +1,112 @@
+import axios from "axios"
+
+  // Remap shopify data to be used in product components
+const transformProduct = (shopifyProduct) => {
+    // console.log('shopifyProduct', shopifyProduct)
+    const { data: { product } } = shopifyProduct
+
+    // Update variant shape
+    product.variants = product.variants.edges.map(({ node }) => {
+        node.price = Number(node?.price?.amount)
+        node.compareAtPrice = Number(node?.compareAtPrice?.amount)
+        node.content = {
+        selectedOptions: node.selectedOptions,
+        featuredMedia: {
+            src: node.image.url
+        }
+        }
+
+        return node
+    })
+
+    // Add back in content field
+    product.content = {
+        title: product.title,
+        handle: product.handle,
+        options: product.options,
+        description: product.description,
+        media: product.media.edges.map(({ node }) => {
+        return {
+            type: node.mediaContentType,
+            src: node.previewImage.url,
+        }
+        })
+    }
+
+    // Clean up 
+    delete product.handle;
+    delete product.title;
+    delete product.options;
+    delete product.description;
+
+    return product
+}
+
+export const getShopifyProduct = (handle) => {
+    const options = {
+      url: process.env.NEXT_PUBLIC_SHOPIFY_ENDPOINT,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Shopify-Storefront-Access-Token': process.env.NEXT_PUBLIC_SHOPIFY_ACCESS_TOKEN
+      },
+      data: JSON.stringify({
+        query: `
+          query {
+            product(handle: "${handle}") {
+              id
+              availableForSale
+              title
+              handle
+              description
+              media(first: 50) {
+                edges {
+                  node {
+                    mediaContentType
+                    previewImage {
+                      url
+                    }
+                  }
+                }
+              }
+              featuredImage {
+                url
+              }
+              options {
+                name
+                values
+              }
+              variants (first: 100) {
+                edges {
+                  node {
+                    availableForSale
+                    quantityAvailable
+                    image {
+                      url
+                    }
+                    price {
+                      amount
+                      currencyCode
+                    }
+                    compareAtPrice {
+                      amount
+                      currencyCode
+                    }
+                    selectedOptions {
+                      name
+                      value
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `
+      })
+    }
+    
+    const shopifyProduct = axios.request(options)
+      .then((response) => transformProduct(response.data))
+
+    return shopifyProduct
+}
